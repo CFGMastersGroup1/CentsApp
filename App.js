5ecbec3b485262d22daf0338b18343f6ba489bdc
Sirad Bihi <sirad@posteo.net>
2022-12-06 23:34:35 +0000

    Remove Native-Base, add tab navigation

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
-import React from "react";
-import { NativeBaseProvider, Text, Box } from "native-base";
-import {customTheme} from "./src/theme/Index";
+import HomeScreen from "./src/Screens/HomeScreen";
+import AccountsSummary from "./src/Screens/AccountsSummary";
+import Transactions from "./src/Screens/Transactions";
+import Budget from "./src/Screens/Budget";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
 
+export default function App(){
+
+  const Tab = createBottomTabNavigator();
+
+  const Stack = createNativeStackNavigator();
+  
   return (
-    <NativeBaseProvider theme={customTheme}>
-      <Box flex={1} bgColor={"blueGray.300"} alignItems="center" justifyContent="center">
-        <Text color={"gray.800"} fontSize={30}>Cents!</Text>
-      </Box>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Accounts Summary" component={AccountsSummary} />
+      <Tab.Screen name="Transactions" component={Transactions} />
+      <Tab.Screen name="Budget" component={Budget} />
+      </Tab.Navigator>
+ 
+      {/* <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About Us" component={AboutUsScreen} />
+      </Stack.Navigator> */}
+    </NavigationContainer>
   );
 }
