5ffac715d21e5c2a69d9c71e2bdc175fa753fd0a
Sirad Bihi <sirad@posteo.net>
2022-12-08 23:32:52 +0000

    Add logo, UI styling

---
+++ app/Screens/LoginScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {StyleSheet} from 'react-native';
+
+import Screen from '../components/Screen'
+
+export default function LoginScreen(props) {
+
+    return(
+        <Screen>
+            <Image source={require("../assets/")} />
+        </Screen>
+
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
